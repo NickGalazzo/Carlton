c712d7991e830ef2fa09e05bed4d74dd4de414c5
nicholas.galazzo <nicholas.galazzo@gmail.com>
2018-12-03 19:51:53 -0500

    added support for jest testing

---
+++ vue/carlton-storybook/tests/unit/example.spec.js
@@ -0,0 +1,10 @@
+//import {mount} from '@vue/test-utils';
+//import Component from '../../src/components/Aggregations/';
+
+describe('Component', () => {
+    test('is a Vue instance', () => {
+        expect(true === true);
+       // const wrapper = mount(Component);
+       // expect(wrapper.isVueInstance()).toBeTruthy();
+    });
+});
