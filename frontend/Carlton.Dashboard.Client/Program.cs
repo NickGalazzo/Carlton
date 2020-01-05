e42b58a822d8ebf82d41b4778623ab54314cfa48
nicholas.galazzo <nicholas.galazzo@gmail.com>
2020-01-05 17:16:29 -0500

    restructure for monorepo

--- frontend/Carlton.Dashboard.Client/Program.cs
+++ frontend/Carlton.Dashboard.Client/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
-namespace BlazorApp1
+namespace Carlton.Dashboard.Client
 {
     public class Program
     {
--- mockserver/apartmentStatus.mock.js
+++
@@ -1,4 +0,0 @@
-var http = require('http');
-var mockserver = require('mockserver');
-
-http.createServer(mockserver('./mocks')).listen(9001);
