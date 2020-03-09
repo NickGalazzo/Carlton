cfa3463331db3d76d9457351a9a76bf08e07dd02
nicholas.galazzo <nicholas.galazzo@gmail.com>
2020-03-08 23:19:08 -0400

    no message

---
+++ frontend/Carlton.TestBed.Client/Shared/TestBedService.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Carlton.Base.Infrastructure.Client.Events;
+using Carlton.Base.Infrastructure.Client.Components.Tree;
+using System;
+
+namespace Carlton.Base.Infrastructure.Client.Components.TestBed
+{
+    public class TestBedService
+    {
+        private TreeItem _selectedItem;
+        public IEnumerable<TreeItem> TreeItems { get; }
+        public TreeItem SelectedItem
+        {
+            get { return _selectedItem; }
+
+            private set
+            {
+
+                _selectedItem = value;
+                TestComponentType = value.Type;
+                TestComponentViewModel = value.ViewModel;
+            }
+        }
+
+        public Type TestComponentType { get; private set; }
+        public object TestComponentViewModel { get; private set; }
+
+        public IList<IComponentEvent> ComponentEvents { get; private set; }
+
+        public TestBedService(IEnumerable<TreeItem> treeItems)
+        {
+            TreeItems = treeItems;
+            ComponentEvents = new List<IComponentEvent>();
+            SelectedItem = treeItems.FirstOrDefault().Children.FirstOrDefault();
+        }
+
+        public void SelectItem(TreeItem item)
+        {
+            SelectedItem = item;
+        }
+
+        public void UpdateTestComponentViewModel(object viewModel)
+        {
+            TestComponentViewModel = viewModel;
+        }
+
+        public void AddComponentEvent(IComponentEvent evt)
+        {
+            ComponentEvents.Add(evt);
+        }
+
+        public void ClearEvents()
+        {
+            ComponentEvents.Clear();
+        }
+    }
+}
---
+++ libs/Carlton.Dashboard.ViewModels/TestViewModels/HomeForDinnerViewModels.cs
@@ -0,0 +1,20 @@
+﻿using Carlton.Dashboard.ViewModels.HomeForDinner;
+using System.Collections.Generic;
+
+namespace Carlton.TestBed.Client.TestViewModels
+{
+    public static class HomeForDinnerViewModels
+    {
+        public static HomeForDinnerViewModel DefaultHomeForDinnerViewModel()
+        {
+            return new HomeForDinnerViewModel()
+            {
+                DinnerGuests = new List<DinnerGuests>()
+                  {
+                        new DinnerGuests("Nick", true, string.Empty),
+                        new DinnerGuests("Steve", false, "Japaneese Class")
+                  }
+            };
+        }
+    }
+}
