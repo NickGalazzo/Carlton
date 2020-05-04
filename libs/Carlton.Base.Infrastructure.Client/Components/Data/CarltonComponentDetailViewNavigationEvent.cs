bb46bcbff5193ac9498b5f3ff8cf74aa7e4dc742
nicholas.galazzo <nicholas.galazzo@gmail.com>
2020-05-03 23:03:56 -0400

    moved menu items out of CarltonParentBase and into the Card component as a template parameter

--- libs/Carlton.Base.Infrastructure.Client/Components/Data/CarltonComponentDetailViewNavigationEvent.cs
+++ libs/Carlton.Base.Infrastructure.Client/Components/Data/CarltonComponentDetailViewNavigationEvent.cs
@@ -1,6 +1,14 @@
-﻿namespace Carlton.Base.Client.Components.Data
+﻿using System;
+
+namespace Carlton.Base.Client.Components.Data
 {
     public class CarltonComponentDetailViewNavigationEvent<TViewModel>
     {
+        public Type Type { get; set; }
+
+        public CarltonComponentDetailViewNavigationEvent()
+        {
+            Type = typeof(TViewModel);
+        }
     }
 }
--- libs/Carlton.Base.Infrastructure.Client/Components/Data/CarltonComponentRefreshEvent.cs
+++ libs/Carlton.Base.Infrastructure.Client/Components/Data/CarltonComponentRefreshEvent.cs
@@ -1,8 +1,14 @@
-﻿
+﻿using System;
 
 namespace Carlton.Base.Client.Components.Data
 {
     public class CarltonComponentRefreshEvent<TViewModel>
     {
+        public Type Type { get; set; }
+
+        public CarltonComponentRefreshEvent()
+        {
+            Type = typeof(TViewModel);
+        }
     }
 }
