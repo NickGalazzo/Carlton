a717f7f067f1900740f87d6adc9723174c1f19e6
nicholas.galazzo <nicholas.galazzo@gmail.com>
2020-11-25 09:32:44 -0500

    refactor

--- frontend/Carlton.TestBed.Client/Shared/ComponentViewer/ViewModel/ComponentViewerViewModel.cs
+++ frontend/Carlton.TestBed.Client/Shared/ComponentViewer/ViewModel/ComponentViewerViewModel.cs
@@ -13,29 +13,4 @@ namespace Carlton.TestBed.Client.Shared.ComponentViewer
         public ComponentStatus ComponentStatus { get; set; }
         public bool IsCarltonComponent { get; set; }
     }
-    public class ComponentViewerViewModelRequest : IRequest<ComponentViewerViewModel>
-    {
-    }
-
-    public class ComponentViewerViewModelHandler : TestBedRequestHandlerViewModelBase<ComponentViewerViewModelRequest, ComponentViewerViewModel>
-    {
-
-        public ComponentViewerViewModelRequestHandler(TestBedState state)
-            : base(state)
-        {
-        }
-
-        public override Task<ComponentViewerViewModel> Handle(ComponentViewerViewModelRequest request, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(new ComponentViewerViewModel
-            {
-                ComponentType = State.TestComponentType,
-                ComponentViewModel = State.TestComponentViewModel,
-                ComponentStatus = State.TestComponentStatus,
-                IsCarltonComponent = true //_state.IsTestComponentCarltonComponent
-                //SelectedNode = _state.TreeItems
-            });
-        }
-    }
-
 }
