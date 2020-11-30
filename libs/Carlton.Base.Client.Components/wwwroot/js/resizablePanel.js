d1a94691f72277a16817ee3b9e9a107d82fef2e4
nicholas.galazzo <nicholas.galazzo@gmail.com>
2020-11-30 17:37:28 -0500

    added resizablePanel control

---
+++ libs/Carlton.Base.Client.Components/wwwroot/js/resizablePanel.js
@@ -0,0 +1,22 @@
+﻿export function initResizablePcanel() {
+    const bar = document.querySelector('.splitter-horizontal');
+    const top = document.querySelector('.panel-top');
+    let mouse_is_down = false;
+
+    bar.addEventListener('mousedown', (e) => {
+        mouse_is_down = true;
+        document
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        if (!mouse_is_down) return;
+
+        top.style.cursor = "row-resize";
+        top.style.height = `${e.clientY}px`;
+    });
+
+    document.addEventListener('mouseup', () => {
+        mouse_is_down = false;
+        top.style.cursor = "default";
+    });
+}
