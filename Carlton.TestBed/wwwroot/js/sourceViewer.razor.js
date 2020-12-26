55a1d1b638a2a370c911d0309269765ba093f14f
nicholas.galazzo <nicholas.galazzo@gmail.com>
2020-12-26 16:30:56 -0500

    changes to sourceViewer to support output markup

--- Carlton.TestBed/wwwroot/js/sourceViewer.razor.js
+++ Carlton.TestBed/wwwroot/js/sourceViewer.razor.js
@@ -2,3 +2,13 @@
     var blocks = document.querySelectorAll('pre code:not(.hljs)');
     Array.prototype.forEach.call(blocks, hljs.highlightBlock);
 };
+
+export function getOutputSource() {
+    var viewer = document.querySelector(".component-viewer");
+    return viewer.innerHTML;
+}
+
+export function setOutputMarkup(markup) {
+    var block = document.querySelector('.test-component-output-source pre code');
+    block.textContent = markup;
+}
